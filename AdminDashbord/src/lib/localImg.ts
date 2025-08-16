1b599178e7e6bd67189f910c5a236173da30a820
yadnesh ranshevare <yranshevare2005@gmail.com>
2025-08-16 16:05:14 +0530

    update file handling for vercel

--- AdminDashbord/src/lib/localImg.ts
+++ AdminDashbord/src/lib/localImg.ts
@@ -6,11 +6,11 @@ try {
     const byte = await image.arrayBuffer();      // It returns a Promise that resolves to an ArrayBuffer representing the contents of a file. An ArrayBuffer is a low-level, fixed-length binary data buffer used in JavaScript to represent raw data in the form of bytes.
     const buffer = Buffer.from(byte)    //you can convert it to a Buffer using Buffer.from().  Buffer is a built-in class in Node.js that allows you to work with binary data directly.
     // const path = join('@','/','public','/','temp',file.name)
-    const path = `./public/temp/${image.name}`
+    const path = `/tmp/${image.name}`
     await writeFile(path,buffer)
-    console.log(`./public/temp/${image.name}`)
+    console.log(`/tmp/${image.name}`)
     return path
 } catch (error) {
-    
+    console.log(error)
 }
 }
